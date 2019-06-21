d2cd5a97c9cfd8729b4ef611954e31a5eff6923d
DaisukeKURATA <d.kruata.38@gmail.com>
2019-06-22 02:05:44 +0900

    ViewModel(ReadModel)を使ったサンプルコードを作成

---
+++ src/main/java/com/github/dkurata38/amazon_payment_example/polymorphysm/application/ContractHistory.java
@@ -0,0 +1,38 @@
+package com.github.dkurata38.amazon_payment_example.polymorphysm.application;
+
+import com.github.dkurata38.amazon_payment_example.common.domain.entity.Contract;
+
+import java.time.LocalDate;
+
+public class ContractHistory implements PurchaseHistory {
+    private final Contract contract;
+
+    public ContractHistory(Contract contract) {
+        this.contract = contract;
+    }
+
+    @Override
+    public LocalDate purchasedDate() {
+        return contract.beginDate;
+    }
+
+    @Override
+    public String purchasedCommodityName() {
+        return contract.service.name;
+    }
+
+    @Override
+    public String unitPrice() {
+        return contract.service.priceByUnitMonth + "円/" + contract.service.unitMonths + "ヶ月" ;
+    }
+
+    @Override
+    public String amount() {
+        return contract.months + "ヶ月";
+    }
+
+    @Override
+    public String subtotal() {
+        return contract.service.priceByUnitMonth / contract.service.unitMonths * contract.months + "円";
+    }
+}
---
+++ src/main/java/com/github/dkurata38/amazon_payment_example/polymorphysm/application/PurchaseService.java
@@ -0,0 +1,33 @@
+package com.github.dkurata38.amazon_payment_example.polymorphysm.application;
+
+import com.github.dkurata38.amazon_payment_example.common.domain.entity.Contract;
+import com.github.dkurata38.amazon_payment_example.common.domain.entity.Purchase;
+import com.github.dkurata38.amazon_payment_example.common.domain.repository.IContractRepository;
+import com.github.dkurata38.amazon_payment_example.common.domain.repository.IPurchaseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class PurchaseService {
+    private final IPurchaseRepository purchaseRepository;
+    private final IContractRepository contractRepository;
+
+    public PurchaseService(IPurchaseRepository purchaseRepository, IContractRepository contractRepository) {
+        this.purchaseRepository = purchaseRepository;
+        this.contractRepository = contractRepository;
+    }
+
+    public List<PurchaseHistory> getMemberPurchaseHistory(Integer memberId) {
+        List<Purchase> purchases = purchaseRepository.findByMemberId(memberId);
+        Stream<CommodityPurchaseHistory> commodityPurchaseHistoryStream = purchases.stream().map(CommodityPurchaseHistory::new);
+        List<Contract> contracts = contractRepository.findByMemberId(memberId);
+        Stream<ContractHistory> contractHistoryStream = contracts.stream().map(ContractHistory::new);
+        return Stream.concat(commodityPurchaseHistoryStream, contractHistoryStream)
+                .sorted(Comparator.comparing(PurchaseHistory::purchasedDate))
+                .collect(Collectors.toList());
+    }
+}
