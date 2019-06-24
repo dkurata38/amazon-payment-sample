aa21994af2659d3ae5ea05b450b560b3efcc8306
DaisukeKURATA <d.kruata.38@gmail.com>
2019-06-24 12:20:28 +0900

    コメントを追加

--- src/main/java/com/github/dkurata38/amazon_payment_example/polymorphysm/application/PurchaseService.java
+++ src/main/java/com/github/dkurata38/amazon_payment_example/polymorphysm/application/PurchaseService.java
@@ -22,10 +22,15 @@ public class PurchaseService {
     }
 
     public List<PurchaseHistory> getMemberPurchaseHistory(Integer memberId) {
+        // 商品購入データを取得し、購入履歴型を実装した商品購入履歴クラスに変換
         List<Purchase> purchases = purchaseRepository.findByMemberId(memberId);
         Stream<CommodityPurchaseHistory> commodityPurchaseHistoryStream = purchases.stream().map(CommodityPurchaseHistory::new);
+
+        // サービス契約データを取得し、購入履歴型を実装したサービス購入履歴クラスに変換
         List<Contract> contracts = contractRepository.findByMemberId(memberId);
         Stream<ServicePurchaseHistory> contractHistoryStream = contracts.stream().map(ServicePurchaseHistory::new);
+
+        // 購入履歴型のリストを結合し、購入日順に並び替え
         return Stream.concat(commodityPurchaseHistoryStream, contractHistoryStream)
                 .sorted(Comparator.comparing(PurchaseHistory::purchasedDate))
                 .collect(Collectors.toList());
